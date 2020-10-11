04f287125b1b7107eb31137673b5129585d4839d
MariamA-A <71216550+MariamA-A@users.noreply.github.com>
2020-10-11 14:13:06 +0400

    Update PSUManager.py

--- PSUManager.py
+++ PSUManager.py
@@ -2,12 +2,18 @@ import Settings
 import Channel
 
 class PSUManager:
+
     userSettingsCH1 = Settings()#get value from UI later
     userSettingsCH2 = Settings()
     userSettingsCH3 = Settings()
-    channel01 = Channel(1, userSettingsCH1)
-    channel02 = Channel(2, userSettingsCH2)
-    channel03 = Channel(3, userSettingsCH3)
+    userSettingsCH1.setAll(0,0,0,0)
+    userSettingsCH2.setAll(0, 0, 0, 0)
+    userSettingsCH3.setAll(0, 0, 0, 0)
+
+
+    channel01 = Channel()
+    channel02 = Channel()
+    channel03 = Channel()
     def _init_(self):
         pass
 
@@ -15,7 +21,18 @@ class PSUManager:
         pass
     def initChannels(self):
         self.channel01.conn()
-        pass
+        self.channel01.reset()
+        self.channel01.set_bias()
+        self.channel01.getuserSettings(self.userSettingsCH1)
+        self.channel02.conn()
+        self.channel02.reset()
+        self.channel02.set_bias()
+        self.channel02.getuserSettings(self.userSettingsCH2)
+        self.channel03.conn()
+        self.channel03.reset()
+        self.channel03.set_bias()
+        self.channel03.getuserSettings(self.userSettingsCH3)
+
     def swichChannelOn(self,id):
         if id == 1:
             self.userSettingsCH1.turn_on(1)
@@ -23,6 +40,7 @@ class PSUManager:
             self.userSettingsCH2.turn_on(2)
         if id == 3:
             self.userSettingsCH3.turn_on(3)
+
     def swichChannelOff(self):
         if id == 1:
             self.userSettingsCH1.turn_off(1)
@@ -36,11 +54,32 @@ class PSUManager:
         pass
     def loadPresets(self):
         pass
-    def readChannels(self):
-        pass
+    def readChannels(self,id):
+        readCH1= Settings()
+        readCH2 = Settings()
+        readCH3 = Settings()
+        if id == 1:
+           readCH1= self.channel01.getreadingsSettings()
+        if id == 2:
+            readCH2 = self.channel02.getreadingsSettings()
+        if id == 3:
+            readCH3 = self.channel03.getreadingsSettings()
+
+        readings = [readCH1, readCH2, readCH3]
+        return readings
+    def userinput(self,v,c,ovp,ocp,id):
+        if id == 1:
+            self.userSettingsCH1.setAll(v,c,ovp,ocp)
+        if id==2:
+            self.userSettingsCH2.setAll(v,c,ovp,ocp)
+        if id ==3:
+            self.userSettingsCH3.setAll(v, c, ovp, ocp)
     def plot(self):
         pass
     def configureChannel(self):
-        pass
+        self.channel01.setuserSettings()
+        self.channel02.setuserSettings()
+        self.channel03.setuserSettings()
+
     def sendToDB(self):
         pass
