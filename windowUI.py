b726a409a4c41a4f0880bcc8e64eccaaee8b32a5
MariamA-A <71216550+MariamA-A@users.noreply.github.com>
2020-10-27 11:07:49 +0400

    Update windowUI.py

--- windowUI.py
+++ windowUI.py
@@ -13,13 +13,15 @@ import time
 import datetime
 import sys
 import os
-
-reading = False
+import threading
+reading = True
+lock = threading.Lock()
 
 class WindowUI(Ui_MainWindow):
     psu = psu.PSUManager()
 
     def __init__(self, window):
+        lock.acquire()
         self.setupUi(window)
 
         ##PSUManager
@@ -73,45 +75,55 @@ class WindowUI(Ui_MainWindow):
         self.currentReadingField_ch1.setText(str(readings[0].getCurr()))
         self.ocpCurrTF_ch1.setText(str(readings[0].getOCP()))
         self.ovpVolTF_ch1.setText(str(readings[0].getOVP()))
+        self.statetReadingField_ch1.setText(str(readings[0].getChannelState()))
+
         self.voltageReadingField_ch2.setText(str(readings[1].getVolt()))
         self.currentReadingField_ch2.setText(str(readings[1].getCurr()))
         self.ocpCurrTF_ch2.setText(str(readings[1].getOCP()))
         self.ovpVolTF_ch2.setText(str(readings[1].getOVP()))
+        self.statetReadingField_ch2.setText(str(readings[1].getChannelState()))
 
         self.voltageReadingField_ch3.setText(str(readings[2].getVolt()))
         self.currentReadingField_ch3.setText(str(readings[2].getCurr()))
         self.ocpCurrTF_ch3.setText(str(readings[2].getOCP()))
         self.ovpVolTF_ch3.setText(str(readings[2].getOVP()))
+        self.statetReadingField_ch3.setText(str(readings[2].getChannelState()))
+
 
     def switchChannels(self):
-        global reading
-        if (reading == False):
+        #global reading
+       # if (reading == False):
 
-            if self.channel1_rBttn.isChecked():
-                self.psu.swichChannelOn(id=1)
-                print("ch1 is on")
+        global lock
+        lock.acquire()
 
-            if self.channel2_rBttn.isChecked():
-                self.psu.swichChannelOn(id=2)
-                print("ch2 is on")
+        if self.channel1_rBttn.isChecked():
+            self.psu.swichChannelOn(id=1)
+            print("ch1 is on")
 
-            if self.channel3_rBttn.isChecked():
-                self.psu.swichChannelOn(id=3)
-                print("ch3 is on")
+        if self.channel2_rBttn.isChecked():
+            self.psu.swichChannelOn(id=2)
+            print("ch2 is on")
 
-            if not self.channel1_rBttn.isChecked():
-                self.psu.swichChannelOff(id=1)
-                print("ch1 is off")
+        if self.channel3_rBttn.isChecked():
+            self.psu.swichChannelOn(id=3)
+            print("ch3 is on")
 
-            if not self.channel2_rBttn.isChecked():
-                self.psu.swichChannelOff(id=2)
-                print("ch2 is off")
+        if not self.channel1_rBttn.isChecked():
+            self.psu.swichChannelOff(id=1)
+            print("ch1 is off")
 
-            if not self.channel3_rBttn.isChecked():
-                self.psu.swichChannelOff(id=3)
-                print("ch3 is off")
+        if not self.channel2_rBttn.isChecked():
+            self.psu.swichChannelOff(id=2)
+            print("ch2 is off")
 
-        else:print("wait Reading")
+        if not self.channel3_rBttn.isChecked():
+            self.psu.swichChannelOff(id=3)
+            print("ch3 is off")
+
+
+
+        #else:print("wait Reading")
 
     def browseFiles(self):
         fname = QFileDialog.getOpenFileNames(None, 'Select preset file', os.getcwd(), 'All Files (*.*)')
@@ -168,9 +180,9 @@ class WindowUI(Ui_MainWindow):
                 'Channel ID: ' + chID + ', Voltage: ' + chVol + ', Current: ' + chCurr + ', OVP: ' + chOVP + ', OCP: ' + chOCP)
         # self.loadF(chID=chID, chVol=chVol, chCurr=chCurr, chOVP=chOVP, chOCP=chOCP)
 
-    def writeFile(self, fn):
-       # dt = str(datetime.datetime.now())
-      #  fn = "preset" + dt + ".txt"
+    def writeFile(self,fn):
+        #dt = str(datetime.datetime.now())
+        #fn = "preset" + dt + ".txt"
         ### getting channel settings
         chVol_1 = str(self.voltageSP_ch1.value())
         chCurr_1 = str(self.currentSP_ch1.value())
@@ -196,9 +208,9 @@ class WindowUI(Ui_MainWindow):
         f.write("3, " + chVol_3 + ", " + chCurr_3 + ", " + chOVP_3 + ", " + chOCP_3)
         f.close()
 
-#     def saveAs(self):
-#         self.writeFile()
-#         print("Save as preset")
+    # def saveAs(self):
+    #     self.writeFile()
+    #     print("Save as preset")
 
     def saveAs(self):
         name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
@@ -210,52 +222,61 @@ class WindowUI(Ui_MainWindow):
         print("Apply selected")
 
     def setCh1(self):
-        global reading
-        if (reading == False):
-            chVol_1 = self.voltageSP_ch1.value()
-            chCurr_1 = self.currentSP_ch1.value()
-            chOVP_1 = self.ovpVolSP_ch1.value()
-            chOCP_1 = self.ocpCurrSP_ch1.value()
+        #global reading
+        #if (reading == False):
+        global lock
+        lock.acquire()
+        chVol_1 = self.voltageSP_ch1.value()
+        chCurr_1 = self.currentSP_ch1.value()
+        chOVP_1 = self.ovpVolSP_ch1.value()
+        chOCP_1 = self.ocpCurrSP_ch1.value()
         # configureChannel(self,v,c,ovp,ocp,id):
 
-            self.psu.configureChannel(chVol_1, chCurr_1, chOVP_1, chOCP_1, 1)
-            print("channel 1 have been set")
-        else:
-            print("wait Reading")
+        self.psu.configureChannel(chVol_1, chCurr_1, chOVP_1, chOCP_1, 1)
+        print("channel 1 have been set")
+
+        #else:
+            #print("wait Reading")
 
     def setCh2(self):
-        global reading
-        if (reading == False):
-            chVol_2 = self.voltageSP_ch2.value()
-            chCurr_2 = self.currentSP_ch2.value()
-            chOVP_2 = self.ovpVolSP_ch2.value()
-            chOCP_2 = self.ocpCurrSP_ch2.value()
+        #global reading
+        #if (reading == False):
+        global lock
+        lock.acquire()
+        chVol_2 = self.voltageSP_ch2.value()
+        chCurr_2 = self.currentSP_ch2.value()
+        chOVP_2 = self.ovpVolSP_ch2.value()
+        chOCP_2 = self.ocpCurrSP_ch2.value()
         # configureChannel(self,v,c,ovp,ocp,id):
 
-            self.psu.configureChannel(chVol_2, chCurr_2, chOVP_2, chOCP_2, 2)
-            print("channel 2 have been set")
-        else:
-            print("wait Reading")
+        self.psu.configureChannel(chVol_2, chCurr_2, chOVP_2, chOCP_2, 2)
+        print("channel 2 have been set")
+
+        #else:
+            #print("wait Reading")
 
     def setCh3(self):
-        global reading
-        if (reading == False):
-            chVol_3 = self.voltageSP_ch3.value()
-            chCurr_3 = self.currentSP_ch3.value()
-            chOVP_3 = self.ovpVolSP_ch3.value()
-            chOCP_3 = self.ocpCurrSP_ch3.value()
+        global lock
+        lock.acquire()
+        #global reading
+        #if (reading == False):
+        chVol_3 = self.voltageSP_ch3.value()
+        chCurr_3 = self.currentSP_ch3.value()
+        chOVP_3 = self.ovpVolSP_ch3.value()
+        chOCP_3 = self.ocpCurrSP_ch3.value()
         # configureChannel(self,v,c,ovp,ocp,id):
 
-            self.psu.configureChannel(chVol_3, chCurr_3, chOVP_3, chOCP_3, 3)
-            print("channel 3 have been set")
-        else:
-            print("wait Reading")
+        self.psu.configureChannel(chVol_3, chCurr_3, chOVP_3, chOCP_3, 3)
+        print("channel 3 have been set")
+
+        #else:
+           # print("wait Reading")
 
 
 class ThreadClass(QtCore.QThread):
     signalExample = QtCore.pyqtSignal(object)
     psu = psu.PSUManager()
-    global reading
+    global lock
 
     def __init__(self):
         super().__init__()
@@ -264,16 +285,18 @@ class ThreadClass(QtCore.QThread):
 
     @QtCore.pyqtSlot()
     def run(self):
-        global reading
+        global lock
         while 1:
-            reading = True
-            print("Reading now")
+            lock.release()
+            #print("Reading now")
             result = self.psu.readChannels()
             self.signalExample.emit(result)
-            reading = False
-            print("ovp:"+result[0].getOVP()+"\n ocp:"+ result[0].getOCP()+"\n curr:"+result[0].getCurr()+"\n vol:"+result[0].getVolt() )
-            print("write now")
-            time.sleep(30)
+            #lock.release()
+            #reading = False
+            #print("ovp:"+result[0].getOVP()+"\n ocp:"+ result[0].getOCP()+"\n curr:"+result[0].getCurr()+"\n vol:"+result[0].getVolt() )
+            #print("write now")
+            #time.sleep(30)
+            #reading = True
 
 
 app = QtWidgets.QApplication(sys.argv)
