9742de45ca4e2544373f2e3de24d2fd4f39ff3a6
MariamA-A <71216550+MariamA-A@users.noreply.github.com>
2020-10-28 13:02:13 +0400

    Update windowUI.py

--- windowUI.py
+++ windowUI.py
@@ -21,7 +21,7 @@ class WindowUI(Ui_MainWindow):
     psu = psu.PSUManager()
 
     def __init__(self, window):
-        lock.acquire()
+        #lock.acquire()
         self.setupUi(window)
 
         ##PSUManager
@@ -63,6 +63,13 @@ class WindowUI(Ui_MainWindow):
         self.setBttn_ch3.clicked.connect(self.setCh3)
         self.switchBttn_channels.clicked.connect(self.switchChannels)
 
+        self.ovpStateBttn_ch1.setChecked(True)
+        self.ovpStateBttn_ch2.setChecked(True)
+        self.ovpStateBttn_ch3.setChecked(True)
+        self.ocpStateBttn_ch1.setChecked(True)
+        self.ocpStateBttn_ch2.setChecked(True)
+        self.ocpStateBttn_ch3.setChecked(True)
+
         self.worker = ThreadClass()
         self.workerThread = QtCore.QThread()
         self.workerThread.started.connect(self.worker.run)
@@ -76,19 +83,24 @@ class WindowUI(Ui_MainWindow):
         self.ocpCurrTF_ch1.setText(str(readings[0].getOCP()))
         self.ovpVolTF_ch1.setText(str(readings[0].getOVP()))
         self.statetReadingField_ch1.setText(str(readings[0].getChannelState()))
+        self.ovpStateTF_ch1.setText(str(readings[0].getOvpS()))
+        self.ocpStateTF_ch1.setText(str(readings[0].getOcpS()))
 
         self.voltageReadingField_ch2.setText(str(readings[1].getVolt()))
         self.currentReadingField_ch2.setText(str(readings[1].getCurr()))
         self.ocpCurrTF_ch2.setText(str(readings[1].getOCP()))
         self.ovpVolTF_ch2.setText(str(readings[1].getOVP()))
         self.statetReadingField_ch2.setText(str(readings[1].getChannelState()))
+        self.ovpStateTF_ch2.setText(str(readings[1].getOvpS()))
+        self.ocpStateTF_ch2.setText(str(readings[1].getOcpS()))
 
         self.voltageReadingField_ch3.setText(str(readings[2].getVolt()))
         self.currentReadingField_ch3.setText(str(readings[2].getCurr()))
         self.ocpCurrTF_ch3.setText(str(readings[2].getOCP()))
         self.ovpVolTF_ch3.setText(str(readings[2].getOVP()))
         self.statetReadingField_ch3.setText(str(readings[2].getChannelState()))
-
+        self.ovpStateTF_ch3.setText(str(readings[2].getOvpS()))
+        self.ocpStateTF_ch3.setText(str(readings[2].getOcpS()))
 
     def switchChannels(self):
         #global reading
@@ -98,27 +110,30 @@ class WindowUI(Ui_MainWindow):
         lock.acquire()
 
         if self.channel1_rBttn.isChecked():
-            self.psu.swichChannelOn(id=1)
+            self.setStateBttn_ch1.setChecked(True)
+            self.psu.switchChannelOn(id=1)
             print("ch1 is on")
 
         if self.channel2_rBttn.isChecked():
-            self.psu.swichChannelOn(id=2)
+            self.setStateBttn_ch2.setChecked(True)
+            self.psu.switchChannelOn(id=2)
             print("ch2 is on")
 
         if self.channel3_rBttn.isChecked():
-            self.psu.swichChannelOn(id=3)
+            self.setStateBttn_ch3.setChecked(True)
+            self.psu.switchChannelOn(id=3)
             print("ch3 is on")
 
         if not self.channel1_rBttn.isChecked():
-            self.psu.swichChannelOff(id=1)
+            self.psu.switchChannelOff(id=1)
             print("ch1 is off")
 
         if not self.channel2_rBttn.isChecked():
-            self.psu.swichChannelOff(id=2)
+            self.psu.switchChannelOff(id=2)
             print("ch2 is off")
 
         if not self.channel3_rBttn.isChecked():
-            self.psu.swichChannelOff(id=3)
+            self.psu.switchChannelOff(id=3)
             print("ch3 is off")
 
 
@@ -128,6 +143,7 @@ class WindowUI(Ui_MainWindow):
     def browseFiles(self):
         fname = QFileDialog.getOpenFileNames(None, 'Select preset file', os.getcwd(), 'All Files (*.*)')
         fpath = fname[0][0]
+        print(fpath)
         self.uploadfileTF.setText(fpath)
         self.readF(fpath)
         print("browse file button")
@@ -200,7 +216,8 @@ class WindowUI(Ui_MainWindow):
         chOCP_3 = str(self.ocpCurrSP_ch3.value())
         ###
 
-        f = open(fn, "w")
+
+        f = open(fn, 'w')
         f.write("1, " + chVol_1 + ", " + chCurr_1 + ", " + chOVP_1 + ", " + chOCP_1)
         f.write("\n")
         f.write("2, " + chVol_2 + ", " + chCurr_2 + ", " + chOVP_2 + ", " + chOCP_2)
@@ -213,9 +230,10 @@ class WindowUI(Ui_MainWindow):
     #     print("Save as preset")
 
     def saveAs(self):
-        name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
-        file = open(name, 'w')
-        self.writeFile(file)
+        name = QFileDialog.getSaveFileName(None, 'Select preset file', os.getcwd(), 'All Files (*.*)')
+        #file = open(name, 'w')
+        #print(name)
+        self.writeFile(name[0]+".txt")
         print("Save as preset")
 
     def applySelected(self):
@@ -230,6 +248,13 @@ class WindowUI(Ui_MainWindow):
         chCurr_1 = self.currentSP_ch1.value()
         chOVP_1 = self.ovpVolSP_ch1.value()
         chOCP_1 = self.ocpCurrSP_ch1.value()
+
+        self.psu.switchChannelOn(id=1) if self.setStateBttn_ch1.isChecked() else self.psu.switchChannelOff(id=1)
+
+        self.psu.switchOcpON() if self.ocpStateBttn_ch1.isChecked() else self.psu.switchOcpOFF()
+
+        self.psu.switchOvpON() if self.ovpStateBttn_ch1.isChecked() else self.psu.switchOvpOFF()
+
         # configureChannel(self,v,c,ovp,ocp,id):
 
         self.psu.configureChannel(chVol_1, chCurr_1, chOVP_1, chOCP_1, 1)
@@ -248,6 +273,11 @@ class WindowUI(Ui_MainWindow):
         chOVP_2 = self.ovpVolSP_ch2.value()
         chOCP_2 = self.ocpCurrSP_ch2.value()
         # configureChannel(self,v,c,ovp,ocp,id):
+        self.psu.switchChannelOn(id=2) if self.setStateBttn_ch2.isChecked() else self.psu.switchChannelOff(id=2)
+
+        self.psu.switchOcpON() if self.ocpStateBttn_ch2.isChecked() else self.psu.switchOcpOFF()
+
+        self.psu.switchOvpON() if self.ovpStateBttn_ch2.isChecked() else self.psu.switchOvpOFF()
 
         self.psu.configureChannel(chVol_2, chCurr_2, chOVP_2, chOCP_2, 2)
         print("channel 2 have been set")
@@ -265,6 +295,11 @@ class WindowUI(Ui_MainWindow):
         chOVP_3 = self.ovpVolSP_ch3.value()
         chOCP_3 = self.ocpCurrSP_ch3.value()
         # configureChannel(self,v,c,ovp,ocp,id):
+        self.psu.switchChannelOn(id=3) if self.setStateBttn_ch3.isChecked() else self.psu.switchChannelOff(id=3)
+
+        self.psu.switchOcpON() if self.ocpStateBttn_ch3.isChecked() else self.psu.switchOcpOFF()
+
+        self.psu.switchOvpON() if self.ovpStateBttn_ch3.isChecked() else self.psu.switchOvpOFF()
 
         self.psu.configureChannel(chVol_3, chCurr_3, chOVP_3, chOCP_3, 3)
         print("channel 3 have been set")
@@ -286,11 +321,16 @@ class ThreadClass(QtCore.QThread):
     @QtCore.pyqtSlot()
     def run(self):
         global lock
+
         while 1:
-            lock.release()
+            try:
+                lock.release()
             #print("Reading now")
-            result = self.psu.readChannels()
-            self.signalExample.emit(result)
+                result = self.psu.readChannels()
+                self.signalExample.emit(result)
+                time.sleep(5)
+            except:
+                lock.acquire()
             #lock.release()
             #reading = False
             #print("ovp:"+result[0].getOVP()+"\n ocp:"+ result[0].getOCP()+"\n curr:"+result[0].getCurr()+"\n vol:"+result[0].getVolt() )
@@ -298,7 +338,7 @@ class ThreadClass(QtCore.QThread):
             #time.sleep(30)
             #reading = True
 
-
+lock.acquire()
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 # whichCommand="asdf"
